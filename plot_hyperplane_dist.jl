3025205ef3f600790fbb5c2341015f69df3a4f56
Adrian Dawid <adriandwd@gmail.com>
2020-08-06 12:08:32 +0200

    Switch to Plots.jl

--- plot_hyperplane_dist.jl
+++ plot_hyperplane_dist.jl
@@ -1,15 +1,24 @@
-using Makie
+using Plots
 
-function plot_plane(center, spanning_vector_a, spanning_vector_b, color=:grey, ε = 1)
-	x = [center[1]-ε*spanning_vector_a[1]-ε*spanning_vector_b[1],center[1]+ε*spanning_vector_a[1]+ε*spanning_vector_b[1],
-	     center[1]-2ε*spanning_vector_b[1],center[1]+2ε*spanning_vector_a[1]]
-	y = [center[2]-ε*spanning_vector_a[2]-ε*spanning_vector_b[2],center[2]+ε*spanning_vector_a[2]+ε*spanning_vector_b[2],
-	     center[2]-2ε*spanning_vector_b[2],center[2]+2ε*spanning_vector_a[2]]
-	z = [center[3]-ε*spanning_vector_a[3]-ε*spanning_vector_b[3],center[3]+ε*spanning_vector_a[3]+ε*spanning_vector_b[3],
-	     center[3]-2ε*spanning_vector_b[3],center[3]+2ε*spanning_vector_a[3]]
+# function plot_plane(center, spanning_vector_a, spanning_vector_b, color=:grey, ε = 1)
+# 	x = [center[1]-ε*spanning_vector_a[1]-ε*spanning_vector_b[1],center[1]+ε*spanning_vector_a[1]+ε*spanning_vector_b[1],
+# 	     center[1]-2ε*spanning_vector_b[1],center[1]+2ε*spanning_vector_a[1]]
+# 	y = [center[2]-ε*spanning_vector_a[2]-ε*spanning_vector_b[2],center[2]+ε*spanning_vector_a[2]+ε*spanning_vector_b[2],
+# 	     center[2]-2ε*spanning_vector_b[2],center[2]+2ε*spanning_vector_a[2]]
+# 	z = [center[3]-ε*spanning_vector_a[3]-ε*spanning_vector_b[3],center[3]+ε*spanning_vector_a[3]+ε*spanning_vector_b[3],
+# 	     center[3]-2ε*spanning_vector_b[3],center[3]+2ε*spanning_vector_a[3]]
+# 
+# 	indices = [1,2,3, 
+# 		   3,2,4]
+# 
+# 	mesh(x, y, z, indices, color = color)
+# end
 
-	indices = [1,2,3, 
-		   3,2,4]
-
-	mesh(x, y, z, indices, color = color)
+function plot_plane(center, spanning_vector_a, spanning_vector_b, color=:grey, ε = 1/2)
+	x = [center[1] - ε * spanning_vector_a[1] - ε * spanning_vector_b[1], center[1] + ε * spanning_vector_a[1] - ε * spanning_vector_b[1]]
+	y = [center[2] - ε * spanning_vector_a[2] - ε * spanning_vector_b[2], center[2] - ε * spanning_vector_a[2] + ε * spanning_vector_b[2]]
+	z = [center[3] - ε * spanning_vector_a[3] - ε * spanning_vector_b[3] center[3] + ε * spanning_vector_a[3] - ε * spanning_vector_b[3]
+	     center[3] - ε * spanning_vector_a[3] + ε * spanning_vector_b[3] center[3] + ε * spanning_vector_a[3] + ε * spanning_vector_b[3]]
+	surface!(x,y,z;color)
 end
+
