5a9151d4667c337d406254fa173ceb003ee3b872
Adrian Dawid <adriandwd@gmail.com>
2020-08-08 12:41:31 +0200

    Fix bug

--- src/plot_hyperplane_dist.jl
+++ src/plot_hyperplane_dist.jl
@@ -1,11 +1,14 @@
 using Plots
+using LinearAlgebra
 
 function plot_plane(center, spanning_vector_a, spanning_vector_b, color=:grey, ε = 1/2)
 	x = [center[1] - ε * spanning_vector_a[1] - ε * spanning_vector_b[1], center[1] + ε * spanning_vector_a[1] + ε * spanning_vector_b[1]]
+	if abs(x[1]-x[2]) < 1e-12 && spanning_vector_a[3]  < 1e-12
+		spanning_vector_a, spanning_vector_b = spanning_vector_b, spanning_vector_a
+	end
 	y = [center[2] - ε * spanning_vector_a[2] - ε * spanning_vector_b[2], center[2] + ε * spanning_vector_a[2] + ε * spanning_vector_b[2]]
-	z = [center[3] - ε * spanning_vector_a[3] - ε * spanning_vector_b[3] center[3] + ε * spanning_vector_a[3] - ε * spanning_vector_b[3]
+	z = [center[3] - ε * spanning_vector_a[3] - ε * spanning_vector_b[3] center[3] + ε * spanning_vector_a[3] - ε * spanning_vector_b[3] 
 	     center[3] - ε * spanning_vector_a[3] + ε * spanning_vector_b[3] center[3] + ε * spanning_vector_a[3] + ε * spanning_vector_b[3]]
-	println(x,y,z)
 	surface!(x,y,z;color)
 end
 
