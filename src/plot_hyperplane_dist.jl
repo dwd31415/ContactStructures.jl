615de36fc1e643fbcf4485de5018dc529cb73bd1
Adrian Dawid <adriandwd@gmail.com>
2020-08-17 22:55:10 +0200

    Change plane respresentative

--- src/plot_hyperplane_dist.jl
+++ src/plot_hyperplane_dist.jl
@@ -2,9 +2,21 @@ using Plots
 using LinearAlgebra
 
 function plot_plane(center, spanning_vector_a, spanning_vector_b, color=:grey, ε = 1/2)
-	x = [center[1] + ε * spanning_vector_a[1],center[1] - ε * spanning_vector_a[1],center[1] + ε * spanning_vector_b[1],center[1] - ε * spanning_vector_b[1]]
-	y = [center[2] + ε * spanning_vector_a[2],center[2] - ε * spanning_vector_a[2],center[2] + ε * spanning_vector_b[2],center[2] - ε * spanning_vector_b[2]]
-	z = [center[3] + ε * spanning_vector_a[3],center[3] - ε * spanning_vector_a[3],center[3] + ε * spanning_vector_b[3],center[3] - ε * spanning_vector_b[3]]
+	x = [center[1] + ε * spanning_vector_a[1] + ε * spanning_vector_b[1],
+	     center[1] - ε * spanning_vector_a[1] + ε * spanning_vector_b[1],
+	     center[1] + ε * spanning_vector_a[1] - ε * spanning_vector_b[1],
+	     center[1] - ε * spanning_vector_a[1] - ε * spanning_vector_b[1]]
+	
+	y = [center[2] + ε * spanning_vector_a[2] + ε * spanning_vector_b[2],
+	     center[2] - ε * spanning_vector_a[2] + ε * spanning_vector_b[2],
+	     center[2] + ε * spanning_vector_a[2] - ε * spanning_vector_b[2],
+	     center[2] - ε * spanning_vector_a[2] - ε * spanning_vector_b[2]]
+
+	z = [center[3] + ε * spanning_vector_a[3] + ε * spanning_vector_b[3],
+	     center[3] - ε * spanning_vector_a[3] + ε * spanning_vector_b[3],
+	     center[3] + ε * spanning_vector_a[3] - ε * spanning_vector_b[3],
+	     center[3] - ε * spanning_vector_a[3] - ε * spanning_vector_b[3]]
+	
 	mesh3d!(x,y,z;color)
 end
 
