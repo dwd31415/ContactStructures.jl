ab400a319c56831d2d798b3cba14fb00233b6f03
Adrian Dawid <adriandwd@gmail.com>
2020-10-03 23:49:00 +0200

    Fix grid bug and add support for markersize

--- src/plotter.jl
+++ src/plotter.jl
@@ -1,6 +1,6 @@
 using Base.Iterators
 
-function plot_along_param_curve!(curve, contact_str, step; midpoint_marker_color = :red, ε = 1.0, frame=false)
+function plot_along_param_curve!(curve, contact_str, step; midpoint_marker_color = :red, markersize = default(:markersize), ε = 1.0, frame=false)
 	ts = collect(0:step:1)
 	points = curve.(ts)
 	plt = nothing
@@ -10,25 +10,25 @@ function plot_along_param_curve!(curve, contact_str, step; midpoint_marker_color
 		basis_b = normalize(basis[2,:])
 		plt = plot_plane([x,y,z],basis_a,basis_b, :grey, ε, frame)
 	end
-	scatter3d!(plt,(x -> x[1]).(points),(x -> x[2]).(points),(x -> x[3]).(points), color=midpoint_marker_color)
+	scatter3d!(plt,(x -> x[1]).(points),(x -> x[2]).(points),(x -> x[3]).(points), color=midpoint_marker_color, markersize = markersize)
 	return plt
 end
 
-function plot_along_param_curve(curve, contact_str, step; midpoint_marker_color = :red, ε = 1.0)
+function plot_along_param_curve(curve, contact_str, step; midpoint_marker_color = :red, markersize = default(:markersize), ε = 1.0)
 	surface()
-	plot_along_param_curve!(curve, contact_str, step; midpoint_marker_color, ε)
+	plot_along_param_curve!(curve, contact_str, step; midpoint_marker_color, markersize, ε)
 end
 
-function plot_along_param_curve!(curve, contact_form :: OneForm, step; midpoint_marker_color = :red, ε = 1.0)
-	plot_along_param_curve!(curve, structuralize_form(contact_form), step; midpoint_marker_color, ε)
+function plot_along_param_curve!(curve, contact_form :: OneForm, step; midpoint_marker_color = :red, markersize = default(:markersize), ε = 1.0)
+	plot_along_param_curve!(curve, structuralize_form(contact_form), step; midpoint_marker_color, markersize, ε)
 end
 
-function plot_along_param_curve(curve, contact_form :: OneForm, step; midpoint_marker_color = :red, ε = 1.0)
-	plot_along_param_curve(curve, structuralize_form(contact_form), step; midpoint_marker_color, ε)
+function plot_along_param_curve(curve, contact_form :: OneForm, step; midpoint_marker_color = :red, markersize = default(:markersize), ε = 1.0)
+	plot_along_param_curve(curve, structuralize_form(contact_form), step; midpoint_marker_color, markersize, ε)
 end
 
-function plot_along_xy_grid!(xs, ys, z, contact_str; midpoint_marker_color = :red, ε = 1.0)
-	grid = collect(product(xs,ys))
+function plot_along_xy_grid!(xs, ys, z, contact_str; midpoint_marker_color = :red, markersize = default(:markersize), ε = 1.0)
+	grid = vcat(collect(product(xs,ys))...)
 	plt = nothing
 	for (x,y) in grid
 		basis = contact_str.distribution(x,y,z)
@@ -36,20 +36,19 @@ function plot_along_xy_grid!(xs, ys, z, contact_str; midpoint_marker_color = :re
 		basis_b = normalize(basis[2,:])
 		plt = plot_plane([x,y,z],basis_a,basis_b, :grey, ε)
 	end
-	print("A")
-#	scatter3d!((x -> x[1]).(grid),(x -> x[2]).(grid),repeat([z], size(grid)...), color=midpoint_marker_color)
+	scatter3d!((x -> x[1]).(grid),(x -> x[2]).(grid),repeat([z], size(grid)...), color=midpoint_marker_color, markersize=markersize)
 	return plt
 end
 
-function plot_along_xy_grid(xs, ys, z, contact_str; midpoint_marker_color = :red, ε = 1.0)
+function plot_along_xy_grid(xs, ys, z, contact_str; midpoint_marker_color = :red, markersize = default(:markersize), ε = 1.0)
 	surface()
-	plot_along_xy_grid!(xs, ys, z, contact_str; midpoint_marker_color, ε)
+	plot_along_xy_grid!(xs, ys, z, contact_str; midpoint_marker_color, markersize, ε)
 end
 
-function plot_along_xy_grid!(xs, ys, z, contact_form::OneForm; midpoint_marker_color = :red, ε = 1.0)
-	plot_along_xy_grid!(xs, ys, z,structuralize_form(contact_form); midpoint_marker_color, ε)
+function plot_along_xy_grid!(xs, ys, z, contact_form::OneForm; midpoint_marker_color = :red, markersize = default(:markersize), ε = 1.0)
+	plot_along_xy_grid!(xs, ys, z,structuralize_form(contact_form); midpoint_marker_color, markersize, ε)
 end
 
-function plot_along_xy_grid(xs, ys, z, contact_form::OneForm; midpoint_marker_color = :red, ε = 1.0)
-	plot_along_xy_grid(xs, ys, z,structuralize_form(contact_form); midpoint_marker_color, ε)
+function plot_along_xy_grid(xs, ys, z, contact_form::OneForm; midpoint_marker_color = :red, markersize = default(:markersize), ε = 1.0)
+	plot_along_xy_grid(xs, ys, z,structuralize_form(contact_form); midpoint_marker_color, markersize, ε)
 end
